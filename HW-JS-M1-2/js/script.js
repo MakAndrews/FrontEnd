21c32b7b0aa79539a0d39eded7e44dff0f3ef79c
Andrey Miroshnichenko <7853637@gmail.com>
2016-03-18 21:30:51 +0200

    JS HW 2: 100%

--- HW-JS-M1-2/js/script.js
+++ HW-JS-M1-2/js/script.js
@@ -1,4 +1,4 @@
-/* 
+/*
 //ÄÇ 1
 var num = prompt ('Enter number');
 var pow = prompt ('Enter pow');
@@ -17,11 +17,18 @@ alert ('Result = ' + result);
 
 
 //ÄÇ 2
-var arr = [prompt ('Enter valid name1'),prompt ('Enter valid name2'),prompt ('Enter valid name3'),prompt ('Enter valid name4'),prompt ('Enter valid name5')]
+var arr=[];
+for (i=0; i < 5; i++) {
+	arr[i]=prompt('Enter valid name ' + i);
+	while (arr[i] =='') {arr[i]=prompt('Re-Enter valid name ' + i);} 
+}
+var i=0;
 var name = prompt ('Now, enter your name');
-var a=0;
-	do 
-		if (name = arr[a]) {alert('Wellcome, ' + arr[a]);} 
-		else {alert('Your name is invalid! Goodbuy!');}
-		a++; //îøèáêà?????
-	while (a<=5);
+while (name =='') {name=prompt('Error! Re-Enter Yor Name');}
+	do {
+		if (name == arr[i]) {alert('Wellcome, ' + arr[i]); break;} 
+		else {i++;
+			if (i == arr.length) {alert('Your name is invalid!'); break;}
+			}
+		}
+	while (i <= --arr.length);
