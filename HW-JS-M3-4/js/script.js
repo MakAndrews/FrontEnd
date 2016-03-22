69ea7c26588963e969ac6fe56ceffc816bba275c
Andrey Miroshnichenko <7853637@gmail.com>
2016-03-22 18:34:43 +0200

    HW JS M3-4: 50%
    Signed-off-by: Andrey Miroshnichenko <7853637@gmail.com>

--- HW-JS-M3-4/js/script.js
+++ HW-JS-M3-4/js/script.js
@@ -1,6 +1,44 @@
-var ElemDiv = document.createElement('div');
-ElemDiv.classList.add('BodyClass');
-ElemDiv.innerHTML = 'Some Text';
+var ElemDivHead = document.createElement('div');
+ElemDivHead.classList.add('HeadClass');
+ElemDivHead.innerHTML = 'Тест по программированию';
 
-var body1 = document.querySelector('body');
-body1.appendChild(ElemDiv);
+var ElemDivBody = document.createElement('div');
+ElemDivBody.classList.add('BodyClass');
+
+var ElemOrderedList = document.createElement('ol');
+ElemOrderedList.classList.add('ListClass');
+
+var List = document.createElement('li');
+var i = 1;
+List.innerHTML = 'Вопрос №' + i;
+
+var CB = document.createElement('input');
+CB.setAttribute('type', 'checkbox');
+CB.setAttribute('value', 'Checkbox_');
+
+var Text = document.createElement('span');
+Text.innerHTML = 'Вариант ответа №' + i, '<br>';
+Text.classList.add('ListClass');
+
+var P = document.createElement('p');
+
+var Button1 = document.createElement('input');
+Button1.setAttribute('type', 'submit');
+Button1.setAttribute('value', 'Отправить ответы');
+Button1.classList.add('ButtonClass');
+
+//
+
+document.body.appendChild(ElemDivHead);
+document.body.appendChild(ElemDivBody);
+ElemDivBody.appendChild(ElemOrderedList);
+List.appendChild(P);
+//ElemOrderedList.appendChild(List)
+
+do {ElemOrderedList.appendChild(List); i++;} while (i<=3);
+i=1;
+do {List.appendChild(CB); i++;} while (i<=3);
+
+List.appendChild(Text);
+
+document.body.appendChild(Button1);
